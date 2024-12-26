42c5bd01682fb533b1c838f1ed936f43edd83b67
William Dalla Stella dos Santos <williamsantosstella@gmail.com>
2024-12-26 20:14:47 -0300

    Project Created

---
+++ src/test/java/com/estudoswill/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudoswill.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

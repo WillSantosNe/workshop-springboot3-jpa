acab8a50c7d6ace31e81f55e4097b949ddb6cf69
WillSantosNe <williamsantosstella@gmail.com>
2025-01-02 18:26:41 -0300

    Payment, one-to-one association

--- src/main/java/com/estudoswill/course/config/TestConfig.java
+++ src/main/java/com/estudoswill/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.estudoswill.course.entities.Category;
 import com.estudoswill.course.entities.Order;
 import com.estudoswill.course.entities.OrderItem;
+import com.estudoswill.course.entities.Payment;
 import com.estudoswill.course.entities.Product;
 import com.estudoswill.course.entities.User;
 import com.estudoswill.course.entities.enums.OrderStatus;
@@ -82,5 +83,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
